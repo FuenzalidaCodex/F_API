99cd259d47a7b70fadb3222379e504382d9b88f7
FuenzalidaCodex <i.fuenzalida@duocuc.cl>
2025-05-15 14:57:20 -0400

    Api rest 1

---
+++ ferremas_api/productos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    codigo = models.CharField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=100)
+    marca = models.CharField(max_length=50)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
