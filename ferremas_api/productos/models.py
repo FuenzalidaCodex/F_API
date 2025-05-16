ed9352816eb0f8eabbb39392e8231c85c3d43cd0
FuenzalidaCodex <i.fuenzalida@duocuc.cl>
2025-05-15 21:00:02 -0400

    up

--- ferremas_api/productos/models.py
+++ ferremas_api/productos/models.py
@@ -1,12 +1,33 @@
 from django.db import models
 
 # Create your models here.
-class Producto(models.Model):
-    codigo = models.CharField(max_length=20, unique=True)
+class Categoria_Producto(models.Model): 
+    id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
-    marca = models.CharField(max_length=50)
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self): 
+        return self.nombre 
+        
+class Ferretería(models.Model):
+    codigo_ferretería = models.AutoField(primary_key=True)
+    nombre_ferretería = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre_ferretería
+
+
+class Producto(models.Model):
+    id_producto=models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50, default="Desconocido")
+    ferretería = models.ForeignKey(Ferretería, on_delete=models.CASCADE, related_name='productos', default=1)
+    categoria = models.ForeignKey(Categoria_Producto, on_delete=models.CASCADE, related_name='productos', default=1)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
     stock = models.IntegerField()
+    descripcion = models.TextField(blank=True, null=True)
+    imagen = models.ImageField(upload_to="productos", null=True, blank=True)
 
     def __str__(self):
         return self.nombre
+
+
+
